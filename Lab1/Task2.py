22a7460592c60d3b20a56159885bfd8a0e76049f
Denis Urakanov|Dev|Home <denisuidev@gmail.com>
2020-09-30 00:00:53 +0500

    update: Task2.py

--- Lab1/Task2.py
+++ Lab1/Task2.py
@@ -11,41 +11,30 @@ import numpy as np
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 2000)
 
-us_list = []
 vg_list = []
+us_cars_list = []
 netflix_list = []
 
-with open('../bank/us.csv', 'r') as f:
-    reader = csv.reader(f)
-    us_list = list(reader)
-    us_list.pop(0)
+# Получение записей видеоигр
+vg_df = pd.read_csv('../bank/vgsales.csv')
+vg_list = vg_df.drop(['Rank', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales'], axis=1).astype(str).values.tolist()
 
-with open('../bank/vgsales.csv', 'r') as f:
-    reader = csv.reader(f)
-    vg_list = list(reader)
-    vg_list.pop(0)
+# Получение Машин США
+us_cars_df = pd.read_csv('../bank/us_cars.csv')
+us_cars_list = us_cars_df.drop([us_cars_df.columns[0], 'mileage', 'vin', 'lot'], axis=1).astype(str).values.tolist()
 
-with open('../bank/netflix_titles.csv', 'r') as f:
-    reader = csv.reader(f)
-    netflix_list = list(reader)
-    netflix_list.pop(0)
-
-# Полезные модификации и трансформации для исходного дата сета
-vg_list = np.delete(vg_list, [6, 7, 8, 9, 10], 1) #Удаляем излишние числовые столбцы
-
-#Заполняем пустые места или null значениями
-trans_netflix_df = pd.read_csv('../bank/netflix_titles.csv')
-trans_netflix_df["director"].fillna("No Director", inplace=True)
-trans_netflix_df = trans_netflix_df.astype(str)
-netflix_list = trans_netflix_df.values.tolist()
+# Получение записей Netflix
+netflix_df = pd.read_csv('../bank/netflix_titles.csv')
+netflix_df["director"].fillna("No Director", inplace=True)
+netflix_list = netflix_df.astype(str).values.tolist()
 
 # Начало трансформации частых наборов
 te = TransactionEncoder()
 vg_te_ary = te.fit(vg_list).transform(vg_list)
 vg_df = pd.DataFrame(vg_te_ary, columns=te.columns_)
 
-us_te_ary = te.fit(us_list).transform(us_list)
-us_df = pd.DataFrame(us_te_ary, columns=te.columns_)
+us_cars_te_ary = te.fit(us_cars_list).transform(us_cars_list)
+us_cars_df = pd.DataFrame(us_cars_te_ary, columns=te.columns_)
 
 netflix_te_ary = te.fit(netflix_list).transform(netflix_list)
 netflix_df = pd.DataFrame(netflix_te_ary, columns=te.columns_)
@@ -53,22 +42,25 @@ netflix_df = pd.DataFrame(netflix_te_ary, columns=te.columns_)
 
 # Apriori
 print('Apriori-Video Games')
-vg_freq_items = apriori(vg_df, min_support=0.001, use_colnames=True).sort_values(by='support', ascending=False)
+vg_freq_items = apriori(vg_df, min_support=0.1, use_colnames=True).sort_values(by='support', ascending=False)
 print(vg_freq_items)
 print('Apriori-US votes')
 start_time = time.time()
-us_freq_items = apriori(us_df, min_support=0.01, use_colnames=True).sort_values(by='support', ascending=False)
-print(us_freq_items)
+us_cars_freq_items = apriori(us_cars_df, min_support=0.01, use_colnames=True).sort_values(by='support', ascending=False)
+print(us_cars_freq_items)
 apriori_time = time.time() - start_time
 print('Apriori-Netflix')
 netflix_freq_items = apriori(netflix_df, min_support=0.1, use_colnames=True).sort_values(by='support', ascending=False)
 print(netflix_freq_items)
 
+print('Association rules for videogames:')
 vg_rules = association_rules(vg_freq_items, metric="confidence", min_threshold=0.6)
 print(vg_rules)
 
-us_freq_items = association_rules(us_freq_items, metric="confidence", min_threshold=0.6)
-print(us_freq_items)
+print('Association rules for US Cars:')
+us_cars_rules = association_rules(us_cars_freq_items, metric="confidence", min_threshold=0.6)
+print(us_cars_rules)
 
+print('Association rules for Netflix:')
 netflix_rules = association_rules(netflix_freq_items, metric="confidence", min_threshold=0.6)
 print(netflix_rules)
