10f534cd0f7c1081d04359a3d94e8f76b6d9a256
Denis Urakanov|Dev <denisuidev@gmail.com>
2020-10-28 17:04:33 +0500

    update: api ping

--- Lab2/Test.py
+++ Lab2/Test.py
@@ -1,21 +1,8 @@
-from matplotlib import pyplot as plt
-from sklearn import datasets
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
-
-# Prepare the data data
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
-
-# Fit the classifier with default hyper-parameters
-clf = DecisionTreeClassifier(random_state=1234)
-model = clf.fit(X, y)
-
-fig = plt.figure(figsize=(25,20))
-_ = tree.plot_tree(clf,
-                   feature_names=iris.feature_names,
-                   class_names=iris.target_names,
-                   filled=True)
-
-fig.savefig("decistion_tree.png")
+import time
+import random
+import requests
+for x in range(20):
+    time.sleep(0.125)
+    docker_host = random.choice(['3.238.72.103', '3.235.198.44', '3.231.228.99'])
+    get_request = requests.get('http://' + str(docker_host))
+    print("Host: {0}, Container: {1}".format(docker_host, get_request))
