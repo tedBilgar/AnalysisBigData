3df9a07f277a6c11ce9b141849aa7d6805852680
Denis Urakanov|Dev|Home <denisuidev@gmail.com>
2020-12-02 16:17:12 +0500

    update: task 7

--- Lab3/Task7.py
+++ Lab3/Task7.py
@@ -6,37 +6,15 @@ from scipy.cluster.hierarchy import dendrogram
 from scipy.cluster.hierarchy import dendrogram, linkage
 
 
-noisy_circles = datasets.make_circles(n_samples=1500, factor=.5, noise=.05)
-noisy_moons = datasets.make_moons(200, noise=0.05, random_state=0)
-blobs = datasets.make_blobs(centers=4, n_samples=200, random_state=0, cluster_std=0.7)
+noisy_circles = datasets.make_circles(n_samples=100, factor=.5, noise=.05)
+noisy_moons = datasets.make_moons(100, noise=0.05, random_state=0)
+blobs = datasets.make_blobs(centers=4, n_samples=100, random_state=0, cluster_std=0.7)
 
 dataset_list = [
     (noisy_circles, {'n_clusters': 2}, 'Circles'),
     (noisy_moons, {'n_clusters': 2}, 'Moons'),
     (blobs, {'n_clusters': 4}, 'Blobs')
 ]
-
-def plot_dendrogram(model, **kwargs):
-    # Create linkage matrix and then plot the dendrogram
-
-    # create the counts of samples under each node
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  # leaf node
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
-
-    linkage_matrix = np.column_stack([model.children_, model.distances_,
-                                      counts]).astype(float)
-
-    # Plot the corresponding dendrogram
-    dendrogram(linkage_matrix, **kwargs)
-
 for (dataset, alg_params, data_name) in dataset_list:
     params = alg_params
     X, y = dataset
@@ -46,18 +24,20 @@ for (dataset, alg_params, data_name) in dataset_list:
     single = AgglomerativeClustering(n_clusters=params['n_clusters'], linkage='single')
 
     clustering_algorithms = (
-            ('Single Linkage', single),
-            ('Average Linkage', average),
-            ('Complete Linkage', complete),
-            ('Ward Linkage', ward),
+            ('Single', single),
+            ('Average', average),
+            ('Complete', complete),
+            ('Ward', ward),
         )
 
     for name, algorithm in clustering_algorithms:
         algorithm.fit(X)
 
         plt.scatter(X[:, 0], X[:, 1], c=algorithm.labels_)
-        plt.title('Визуализация для датасета ' + str(data_name) + ' алгоритмом ' + str(name))
+        plt.title('Визуализация для датасета ' + str(data_name) + ' Linkage алгоритмом ' + str(name))
         plt.show()
 
-        plot_dendrogram(algorithm, truncate_mode='level', p=3)
+        Z = linkage(X, method=str(name).lower())
+        dendrogram(Z)
+        plt.show()
 
