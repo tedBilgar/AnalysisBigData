678851faac3c21715577cb615cf561be45e17a37
Denis Urakanov|Dev <denisuidev@gmail.com>
2020-12-08 18:38:14 +0500

    update: task8

--- Lab3/Task8_1.py
+++ Lab3/Task8_1.py
@@ -5,7 +5,7 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 
 
-# Строим выпуклые кластеры
+# Строим выпуклые кластеры 1 Вариант
 X_blobs,y_blobs = make_blobs(centers=3, n_samples=200, random_state=0, cluster_std=0.4)
 plt.scatter(X_blobs[:,0],X_blobs[:,1])
 plt.show()
@@ -18,5 +18,16 @@ visualizer = KElbowVisualizer(model, k=(1,10))
 visualizer.fit(X_blobs)        # Fit the data to the visualizer
 visualizer.show()        # Finalize and render the figure
 
+
+# Строим выпуклые кластеры 2 Вариант
+X_blobs,y_blobs = make_blobs(centers=3, n_samples=200, random_state=0, cluster_std=0.8)
+plt.scatter(X_blobs[:,0],X_blobs[:,1])
+plt.show()
+
+model = KMeans()
+visualizer = KElbowVisualizer(model, k=(1,10))
+
+visualizer.fit(X_blobs)
+visualizer.show()
 
 
